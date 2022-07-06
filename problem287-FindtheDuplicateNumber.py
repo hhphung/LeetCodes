9407bf16cddcbe523c43c70d326b7851af694dc9
hhphung <97710145+hhphung@users.noreply.github.com>
2022-07-05 22:12:44 -0500

    Create problem287-FindtheDuplicateNumber.py

---
+++ problem287-FindtheDuplicateNumber.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        for i in range(0, len(nums) -1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
